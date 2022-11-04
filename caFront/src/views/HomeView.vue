15b8e87eb66c001cde8fbddadfa23a5e04c88fe1
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-11-04 12:30:16 -0400

    Copy & Paste

--- caFront/src/views/HomeView.vue
+++ caFront/src/views/HomeView.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
         This is the Home Vue
+    <div>
+        <div id="g_id_onload"
+         data-client_id="696624175161-fjet2s1pbefuqnmn42o65p33b5tqa5p1.apps.googleusercontent.com"
+         data-login_uri="https://localhost:5173"
+         data-auto_prompt="false">
+      </div>
+      <div class="g_id_signin"
+         data-type="standard"
+         data-size="large"
+         data-theme="outline"
+         data-text="sign_in_with"
+         data-shape="rectangular"
+         data-logo_alignment="left">
+      </div>
+    </div>
+
+        <div>
+            Login
+
+            <button>
+                Google
+            </button>
+        </div>
     </div>
 </template>
 
