68bd569be22992a1c78b847781bbf1aaf7abbf5e
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-11-16 12:25:29 -0500

    a

--- caFront/src/views/HomeView.vue
+++ caFront/src/views/HomeView.vue
@@ -1,36 +1,66 @@
 <template>
-    <div>
-        This is the Home Vue
+ 
+ 
     <div>
         <div id="g_id_onload"
          data-client_id="696624175161-fjet2s1pbefuqnmn42o65p33b5tqa5p1.apps.googleusercontent.com"
          data-login_uri="https://localhost:5173"
-         data-auto_prompt="false">
+         data-auto_prompt="true">
       </div>
-      <div class="g_id_signin"
+      <div class="g_id_signin " ref="googleBtn"
          data-type="standard"
          data-size="large"
          data-theme="outline"
          data-text="sign_in_with"
-         data-shape="rectangular"
+         data-shape="pill"
+         data-width="400"
          data-logo_alignment="left">
       </div>
-    </div>
 
-        <div>
-            Login
+      <h2>Name {{name}} = {{num}}</h2>
 
-            <button>
-                Google
-            </button>
-        </div>
+      <button @click="addNum(num)" >Sheesh</button>
+
+
+
+
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent, reactive, toRefs, ref} from 'vue';
+
+export default defineComponent({
+
+    setup(){
+        const state = reactive({
+            name: "The Name",
+            num: 31 as number,
+            googleBTN: ref(null)
+            
+        })
+        return{
+            ...toRefs(state)
+        }
+    },
+    methods: {
+        addNum(){
+            this.$refs["googleBtn"].click()
+    
+        }
+
+    }
+
+   
+})
+
 
 </script>
 
 <style scoped>
+.googleBtn{
+    display: none;
+}
+
 
 </style>
