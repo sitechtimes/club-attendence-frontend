56c25a0e174495b1aead97e01521c3e38da39934
Michael Chen <pokemondinner321@gmail.com>
2022-11-16 20:09:50 -0500

    I made it sync with google button

--- caFront/src/views/HomeView.vue
+++ caFront/src/views/HomeView.vue
@@ -1,66 +1,55 @@
 <template>
- 
- 
-    <div>
-        <div id="g_id_onload"
-         data-client_id="696624175161-fjet2s1pbefuqnmn42o65p33b5tqa5p1.apps.googleusercontent.com"
-         data-login_uri="https://localhost:5173"
-         data-auto_prompt="true">
-      </div>
-      <div class="g_id_signin " ref="googleBtn"
-         data-type="standard"
-         data-size="large"
-         data-theme="outline"
-         data-text="sign_in_with"
-         data-shape="pill"
-         data-width="400"
-         data-logo_alignment="left">
-      </div>
+  <div>
+    <div
+      id="g_id_onload"
+      data-client_id="696624175161-fjet2s1pbefuqnmn42o65p33b5tqa5p1.apps.googleusercontent.com"
+      data-login_uri="https://localhost:5173"
+      data-auto_prompt="true"
+    ></div>
+    <div
+      class="g_id_signin"
+      data-type="standard"
+      data-size="large"
+      data-theme="outline"
+      data-text="sign_in_with"
+      data-shape="pill"
+      data-width="400"
+      data-logo_alignment="left"
+      ref="googleBtn"
+    ></div>
 
-      <h2>Name {{name}} = {{num}}</h2>
+    <h2>Name {{ state.name }} = {{ state.num }}</h2>
 
-      <button @click="addNum(num)" >Sheesh</button>
-
-
-
-
-    </div>
+    <button @click="clickGoogle">Sheesh</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs, ref} from 'vue';
+import { defineComponent, onMounted, reactive, ref } from "vue";
 
 export default defineComponent({
-
-    setup(){
-        const state = reactive({
-            name: "The Name",
-            num: 31 as number,
-            googleBTN: ref(null)
-            
-        })
-        return{
-            ...toRefs(state)
-        }
-    },
-    methods: {
-        addNum(){
-            this.$refs["googleBtn"].click()
-    
-        }
-
+  setup() {
+    const googleBtn = ref<HTMLElement | null>(null);
+    const state = reactive({
+      name: "The Name",
+      num: 31 as number,
+    });
+    function clickGoogle() {
+      googleBtn.value?.firstChild?.firstElementChild?.firstElementChild.click();
     }
+    return {
+      state,
+      googleBtn,
+      clickGoogle,
+    };
+  },
 
-   
-})
-
-
+  methods: {},
+});
 </script>
 
 <style scoped>
-.googleBtn{
-    display: none;
+.googleBtn {
+  display: none;
 }
-
-
 </style>
