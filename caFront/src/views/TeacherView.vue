f3c8439d6d4096026244d88fe631dddfe8a70846
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2022-11-21 12:31:25 -0500

    Fake Data

---
+++ caFront/src/views/TeacherView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2 v-for="student in data" key="student" >{{student}}</h2>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core";
+import fakeData from "../assets/fakedata.json"
+
+export default defineComponent({
+    setup(){
+        const data = fakeData
+
+        return{
+            data
+        }
+    }
+
+
+})
+</script>
+
+<style scoped>
+
+</style>
