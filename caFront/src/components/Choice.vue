329ed9c26703cdf35ec4119bca1ecb56ba5e2851
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2022-11-21 22:21:03 -0500

    dawd

---
+++ caFront/src/components/Choice.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>Next Meeting: 12/03/2022</h2>
+        <Button class="edit">Edit</Button>
+        <Button class="remove">Remove</Button>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from '../components/Button.vue'
+import {defineComponent} from 'vue'
+    export default defineComponent ({
+        name: 'Choice',
+        components:{
+            Button
+        }
+    })
+</script>
+
+<style scoped>
+.remove{
+    background-color: #EE4B2B;
+}
+</style>
