181c4e5cbb3623f62c1830dea8a8439f3535cc09
HaoChenSupreme <90791814+HaoChenSupreme@users.noreply.github.com>
2022-12-23 12:13:25 -0500

    added enter club code

--- caFront/src/views/QRCode.vue
+++ caFront/src/views/QRCode.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>This is about page</div>
-  <QRScanner />
+  <div class="box">
+    <h2>Add Club</h2>
+    <form id="form" @submit.prevent="signInButtonPressed">
+      <label for="fname">Input Code: </label>
+      <input type="text" required id="name" />
+    </form>
+  </div>
 </template>
 
-<script setup lang="ts">
-import QRScanner from "../components/QRScanner.vue";
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "About",
+  components: {},
+  methods: {
+    signInButtonPressed(event) {
+      console.log("Sign In Button Pressed");
+      event.preventDefault();
+      event.target.reset();
+    },
+  },
+});
 </script>
 
-<style scoped></style>
+<style>
+.box {
+  height: 25rem;
+  width: 35rem;
+  border: 5px solid black;
+  border-radius: 1rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#form {
+  margin-top: 2rem;
+  font-size: 3rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
