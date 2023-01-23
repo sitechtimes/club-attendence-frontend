7cabe78d14d7365d253cff0a3d50d3e91229b182
EricWeng23 <ericw39@nycstudents.net>
2023-01-23 12:29:56 -0500

    fesf

---
+++ caFront/src/components/icons/Camera.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Camera",
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
