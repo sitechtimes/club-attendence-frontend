853ece1a98f766f5a868f8a464d9ebad78fa0503
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2023-02-02 12:12:36 -0500

    dawd

--- caFront/src/components/Camera.vue
+++ caFront/src/components/Camera.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <qrcode-stream></qrcode-stream>
+    <qrcode-stream @decode="onDecode" @init="onInit" />
+    <h2 class="decode-result">
+      Last result: <b>{{ result }}</b>
+    </h2>
+    <h2 class="error">{{ error }}</h2>
+
     <miniButton
       class="position"
       @click="clubActivity.closeCamera()"
@@ -25,6 +30,42 @@ export default defineComponent({
     QrcodeStream,
     QrcodeDropZone,
     QrcodeCapture,
+  },
+  data() {
+    return {
+      result: "",
+      error: "",
+    };
+  },
+  methods: {
+    onDecode(result) {
+      this.result = result;
+    },
+
+    async onInit(promise) {
+      try {
+        await promise;
+      } catch (error) {
+        if (error.name === "NotAllowedError") {
+          this.error = "ERROR: you need to grant camera access permission";
+        } else if (error.name === "NotFoundError") {
+          this.error = "ERROR: no camera on this device";
+        } else if (error.name === "NotSupportedError") {
+          this.error = "ERROR: secure context required (HTTPS, localhost)";
+        } else if (error.name === "NotReadableError") {
+          this.error = "ERROR: is the camera already in use?";
+        } else if (error.name === "OverconstrainedError") {
+          this.error = "ERROR: installed cameras are not suitable";
+        } else if (error.name === "StreamApiNotSupportedError") {
+          this.error = "ERROR: Stream API is not supported in this browser";
+        } else if (error.name === "InsecureContextError") {
+          this.error =
+            "ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
+        } else {
+          this.error = `ERROR: Camera error (${error.name})`;
+        }
+      }
+    },
   },
 
   setup() {
