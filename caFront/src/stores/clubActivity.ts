3b2518b7d8487d4c3b6b38679be47a65c1725ba9
EricWeng23 <ericw39@nycstudents.net>
2023-02-02 12:22:32 -0500

    fqwf

--- caFront/src/stores/clubActivity.ts
+++ caFront/src/stores/clubActivity.ts
@@ -18,6 +18,7 @@ export const useClubActivity = defineStore("clubActivity", {
     showModal() {
       this.isModalVisible = true;
       this.isPanelVisible = false;
+      this.isCameraVisible = false;
     },
     closeModal() {
       this.isModalVisible = false;
@@ -26,12 +27,16 @@ export const useClubActivity = defineStore("clubActivity", {
       this.isPanelVisible = true;
       this.isModalVisible = false;
       this.isOpen = false;
+      this.isCameraVisible = false;
+
     },
     closePanel() {
       this.isPanelVisible = false;
     },
     openCamera() {
       this.isCameraVisible = true;
+      this.isModalVisible = false;
+      this.isPanelVisible = false;
     },
     closeCamera() {
       this.isCameraVisible = false;
