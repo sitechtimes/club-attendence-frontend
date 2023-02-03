cf77ee0ebc4312e6ddcb8e0840c5f8aff4f206ac
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2023-02-03 12:32:21 -0500

    seegal

--- caFront/src/views/AboutView.vue
+++ caFront/src/views/AboutView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="stevenHomePagePOG">
-    <section class="left" > </section>
+    <section class="left" > 
+      <img class="see"  src="../assets/logos/Seegal.png" alt="">
+
+    </section>
     <section class="right" >
       <div class="box">
-        <h2 class="title">Staten Island Technical High School Club Attendance</h2>
+        <div class="title">Staten Island Technical High School Club Attendance</div>
 
           <button class="OAuth" @click="login">
             <img
@@ -11,7 +14,7 @@
               src="../components/icons/google-logo.png"
               alt="g-logo"
             />
-            <h2>Sign In With Google</h2>
+            <h3>Sign In With Google</h3>
 
           </button>
   
@@ -92,18 +95,32 @@ export default defineComponent({
 
 <style scoped>
 
-.OAuth{
+
+.see{
+  position: relative;
+  z-index: 2;
+}
+.box{
+  width: 40vw;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 50%;
+  font-size: 5vw;
+}
+/* .OAuth{
   display: flex;
   align-items: center;
   justify-content: space-around;
   border-radius: 0.5rem;
   border-width: 0.1rem;
   background-color: white;
-  box-shadow:  5px 1px 100px 1px;
+  box-shadow:  7px 10px 10px grey ;
 }
 .g-logo{
   height: 5rem;
-}
+} */
 .stevenHomePagePOG{
   display: flex;
   position: relative;
@@ -113,6 +130,8 @@ export default defineComponent({
   background-color: #F3C87C;
   height: 100vh;
   width: 40vw;
+  display: flex;
+  justify-content: center;
 }
 .right{
   height: 100vh;
@@ -142,6 +161,61 @@ top: 50%;
   top: -20%;
   right: -50%;
 
+}
+
+
+.OAuth {
+  margin-top: 5rem;
+  height: 50rem;
+  border-radius: 5rem;
+  width: 40rem;
+  background-color: rgb(34, 34, 34);
+  color: rgb(255, 255, 255);
+  animation: float 3s ease-in-out infinite;
+}
+.OAuth h2 {
+  position: relative;
+  width: 77%;
+  margin: auto;
+}
+
+.OAuth h2:before {
+  content: "";
+  position: absolute;
+  width: 0%;
+  height: 0.2rem;
+  border-radius: 20rem;
+  background-color: white;
+  bottom: 0;
+  left: 0;
+  transition: all 0.5s;
+}
+
+.OAuth h2 {
+  font-size: 3rem;
+  font-weight: 400;
+}
+.g-logo {
+  height: 50%;
+}
+
+.OAuth:hover {
+  background-color: rgb(22, 22, 22);
+}
+.OAuth:hover h2:before {
+  width: 100%;
+}
+
+@keyframes float {
+  0% {
+    transform: translate(0, 0px);
+  }
+  50% {
+    transform: translate(0, 20px);
+  }
+  100% {
+    transform: translate(0, 0px);
+  }
 }
 
 
