7612f7721a0cb3925bebae07b8001c253a2d0ce0
Ericweng2023 <95102206+Ericweng2023@users.noreply.github.com>
2023-02-07 18:53:44 -0500

    dawdw

--- caFront/src/components/Camera.vue
+++ caFront/src/components/Camera.vue
@@ -1,72 +1,33 @@
 <template>
   <div>
-    <qrcode-stream @decode="onDecode" @init="onInit" />
+    <qrcode-stream />
     <h2 class="decode-result">
       Last result: <b>{{ result }}</b>
     </h2>
-    <h2 class="error">{{ error }}</h2>
 
-    <miniButton
-      class="position"
-      @click="clubActivity.closeCamera()"
-    >
-
-  </miniButton>
+    <miniButton class="position" @click="clubActivity.closeCamera()">
+    </miniButton>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Camera from "simple-vue-camera";
-import {
-  QrcodeStream,
-  QrcodeDropZone,
-  QrcodeCapture,
-} from "vue3-qrcode-reader";
+import { QrcodeStream } from "vue-qrcode-reader";
 import { useClubActivity } from "../stores/clubActivity";
 import miniButton from "../components/miniButton.vue";
 export default defineComponent({
   components: {
-    Camera,
     miniButton,
     QrcodeStream,
-    QrcodeDropZone,
-    QrcodeCapture,
   },
   data() {
     return {
       result: "",
-      error: "",
     };
   },
   methods: {
-    onDecode(result) {
+    onDecode(result: string) {
       this.result = result;
-    },
-
-    async onInit(promise) {
-      try {
-        await promise;
-      } catch (error) {
-        if (error.name === "NotAllowedError") {
-          this.error = "ERROR: you need to grant camera access permission";
-        } else if (error.name === "NotFoundError") {
-          this.error = "ERROR: no camera on this device";
-        } else if (error.name === "NotSupportedError") {
-          this.error = "ERROR: secure context required (HTTPS, localhost)";
-        } else if (error.name === "NotReadableError") {
-          this.error = "ERROR: is the camera already in use?";
-        } else if (error.name === "OverconstrainedError") {
-          this.error = "ERROR: installed cameras are not suitable";
-        } else if (error.name === "StreamApiNotSupportedError") {
-          this.error = "ERROR: Stream API is not supported in this browser";
-        } else if (error.name === "InsecureContextError") {
-          this.error =
-            "ERROR: Camera access is only permitted in secure context. Use HTTPS or localhost rather than HTTP.";
-        } else {
-          this.error = `ERROR: Camera error (${error.name})`;
-        }
-      }
     },
   },
 
---
+++ caFront/src/components/QRcode.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <StreamBarcodeReader
+      @decode="onDecode"
+      @loaded="onLoaded"
+    ></StreamBarcodeReader>
+    <h2>{{ decodedText }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ref } from "vue";
+import { StreamBarcodeReader } from "vue-barcode-reader";
+export default defineComponent({
+  name: "QRcode",
+  component: {
+    StreamBarcodeReader,
+  },
+  setup() {
+    const decodedText = ref("");
+    const onLoaded = () => {
+      console.log("loaded");
+    };
+    const onDecode = (text) => {
+      decodedText.value = text;
+    };
+    return { onLoaded, onDecode, decodedText };
+  },
+});
+</script>
+
+<style scoped></style>
