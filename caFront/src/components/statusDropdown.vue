099a049459f0561f7dcd26baf95145dffbe10053
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2023-04-28 12:32:41 -0400

    :skull:

--- caFront/src/components/statusDropdown.vue
+++ caFront/src/components/statusDropdown.vue
@@ -1,8 +1,11 @@
-<template>
+<template  >
+
+
     <button class="dropdown" @click="store.statusButton = !store.statusButton" >{{ store.statusFilterCurrent }}</button>
     <div v-if="store.statusButton" class="status">
         <li class="choices" v-for="stat in status" @click="store.filterStatus(stat)" >{{ stat }}</li>
     </div>
+    
 </template>
 
 <script lang="ts">
@@ -20,48 +23,51 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.dropdown {
-    position: relative;
-    display: inline-block;
-    background-color: #ddd;
-    height: 7rem;
-    width: 10rem;
-    color: #444;
-    font-size: 16px;
-    border: none;
-    padding: 10px;
-    border-radius: 4px 4px 4px 4px;
-    margin-right: 1rem;
-   
-  }
 
-  
-  .choices-container {
-    
-    position: absolute;
-    top: 100%;
-    left: 0;
-    width: 100%;
-    max-height: 200px;
-    overflow-y: auto;
-    background-color: #fff;
-    box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-    z-index: 1;
-    border-radius: 0 0 4px 4px;
-    padding: 0;
-    margin: 0;
-  }
-  
-  .choices {
-    padding: 10px;
-    font-size: 16px;
-    color: #444;
-    cursor: pointer;
-    list-style-type: none;
-  }
-  
-  .choices:hover {
-    background-color: #f2f2f2;
-  }
-  
+
+.dropdown {
+  width: 10rem;
+  height: 4rem;
+  padding: 1rem;
+  text-align: center;
+  background: #FFFFFF;
+  magin:auto;
+border: 1px solid #D0D0D0;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+display: flex;
+align-items: center;
+
+
+}
+
+.status{
+  text-align: center;
+  align-items: center;
+}
+/* CSS */
+/* button {
+  align-items: center;
+  background: #FFFFFF;
+  border: 0 solid #E2E8F0;
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  box-sizing: border-box;
+  color: #1A202C;
+  display: inline-flex;
+  font-family: Inter, sans-serif;
+  font-size: 1rem;
+  font-weight: 700;
+  height: 56px;
+  justify-content: center;
+  line-height: 24px;
+  overflow-wrap: break-word;
+  padding: 24px;
+  text-decoration: none;
+  width: auto;
+  border-radius: 8px;
+  cursor: pointer;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+} */
 </style>
