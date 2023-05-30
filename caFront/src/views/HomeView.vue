f916f194ba89f1c90fac62f870fdb2cb8d284597
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-05-29 21:33:08 -0400

    Update HomeView.vue

--- caFront/src/views/HomeView.vue
+++ caFront/src/views/HomeView.vue
@@ -64,13 +64,13 @@ export default defineComponent({
           console.log("Success:", data);
           if (data.clientAuthority === "student") {
             userDataStore.addUserData(data);
-            if (data.osis === "none") {
+            if (data.osis === null) {
               return router.push("/additional-information/osis");
             }
-            if (data.grade === "none") {
+            if (data.grade === null) {
               return router.push("/additional-information/grade");
             }
-            if (data.officalClass === "none") {
+            if (data.officalClass === null) {
               return router.push("/additional-information/offical-class");
             }
             return router.push("/club");
