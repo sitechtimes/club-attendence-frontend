919466241876539f416b81711c128b8f9cbde692
Michael Chen <73071479+Blanku-cloud@users.noreply.github.com>
2023-06-02 09:45:19 -0400

    ew

--- caFront/src/main.ts
+++ caFront/src/main.ts
@@ -19,8 +19,7 @@ app.use(VCalendar, {});
 
 //clientId need to be made be imported from env file
 app.use(vue3GoogleLogin, {
-  clientId:
-    "696624175161-fjet2s1pbefuqnmn42o65p33b5tqa5p1.apps.googleusercontent.com",
+  clientId: `${import.meta.env.VITE_GOOGLE_CLIENT_ID}`,
 });
 
 app.mount("#app");
